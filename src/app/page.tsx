de5446f0570aba52717b14b3f1dac1377a86031c
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-02 23:55:37 +0600

    feat(auth): implement authentication and middleware using Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello Home Page</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-blue-500 px-4 py-2 rounded-md">Sign In</button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
   );
 }
