d037ff14d83e1571d15e653f989b292de0454ac5
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-04-03 03:54:25 +0600

    feat(nav): implement responsive navbar and mobile navigation sheet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <ModeToggle></ModeToggle>
+      Home Page
     </div>
   );
 }
